138b814398041b64ec5a5d658f2fc227952c289e
Sivabudh Umpudh <sivabudh.u@codium.co>
2019-07-30 00:11:18 +0700

    Made number of lines per file configurable

--- split_stdin_to_files.py
+++ split_stdin_to_files.py
@@ -1,14 +1,20 @@
 import sys
+import argparse
 from toolz import itertoolz
 
 '''
 Program #1
-Input: cat myfiles.txt | python split_lines_to_files.py
+Input: cat myfiles.txt | python split_stdin_to_files.py 1000
 Output: Split into N files where each file is 10K files long. Last file will be remainder
 '''
 
+parser = argparse.ArgumentParser(description='Upload files to S3')
+parser.add_argument('num_filenames_per_file', metavar='num_filenames_per_file', type=int,
+                   help="Number of file names per file.")
+args = parser.parse_args()
+
 filenames = sys.stdin.readlines()
-files_list = itertoolz.partition_all(10000, filenames)
+files_list = itertoolz.partition_all(args.num_filenames_per_file, filenames)
 
 for index, files in enumerate(files_list, start=0):
     outfilename = f"./output/{index}"
