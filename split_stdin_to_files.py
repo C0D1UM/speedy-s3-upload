323d1fa398ab62bcf0c29c7e04dedf34fc00760e
Sivabudh Umpudh <sivabudh.u@codium.co>
2019-07-29 23:54:17 +0700

    Added comments

--- split_stdin_to_files.py
+++ split_stdin_to_files.py
@@ -1,6 +1,12 @@
 import sys
 from toolz import itertoolz
 
+'''
+Program #1
+Input: cat myfiles.txt | python split_lines_to_files.py
+Output: Split into N files where each file is 10K files long. Last file will be remainder
+'''
+
 filenames = sys.stdin.readlines()
 files_list = itertoolz.partition_all(10000, filenames)
 
