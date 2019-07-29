bcafb9db40b3d555ee706e63c40b75ab421c41ea
Sivabudh Umpudh <sivabudh.u@codium.co>
2019-07-29 23:31:49 +0700

    Refactored code

--- split_lines_to_files.py
+++ split_lines_to_files.py
@@ -2,8 +2,9 @@ import sys
 from toolz import itertoolz
 
 filenames = sys.stdin.readlines()
-files_list = list(itertoolz.partition_all(10000, filenames))
+files_list = itertoolz.partition_all(10000, filenames)
 
 for index, files in enumerate(files_list, start=0):
-    with open(f"./output/{str(index)}", 'w') as outfile:
+    outfilename = f"./output/{index}"
+    with open(outfilename, 'w') as outfile:
         outfile.writelines(files)
