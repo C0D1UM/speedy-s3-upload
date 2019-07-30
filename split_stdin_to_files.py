50123be6f04e8272e0ef8f44a10a61a3bb38586a
Sivabudh Umpudh <sivabudh.u@codium.co>
2019-07-30 13:34:38 +0700

    Revised program's comments

--- split_stdin_to_files.py
+++ split_stdin_to_files.py
@@ -5,10 +5,10 @@ from toolz import itertoolz
 '''
 Program #1
 Input: cat myfiles.txt | python split_stdin_to_files.py 1000
-Output: Split into N files where each file is 10K files long. Last file will be remainder
+Output: Split into N files where each file is M files long where M is the script parameter. Last file will be remainder
 '''
 
-parser = argparse.ArgumentParser(description='Upload files to S3')
+parser = argparse.ArgumentParser(description='Split file lines into multiple files. The input argument sets the number of ')
 parser.add_argument('num_filenames_per_file', metavar='num_filenames_per_file', type=int,
                    help="Number of file names per file.")
 args = parser.parse_args()
